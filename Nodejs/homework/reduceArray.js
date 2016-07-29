83c10a2e7c08b155e387755f77d8e68843abf5f9
jiangweigithub <wei.jiang@winsuntech.cn>
2016-07-29 23:54:21 +0800

    modified 'reduceArray.js'

--- Nodejs/homework/reduceArray.js
+++ Nodejs/homework/reduceArray.js
@@ -1,16 +1,34 @@
-let testData = [[1,2,3],[4,5],[6],[7,8,9],[10,11,12,13],[14,15,16],[17,18][19,20],[21,22,23,24]];
+let testData = [[1,2,3],[4,5],[6],[7,8,9],[10,11,12,13],[14,15,16],[17,18],[19,20],[21,22,23,24],[25,26],[27],[28,29,30]]
 
-let test = [1,2,3,4,5,6,7,8,9];
+let test = [1,2,3,4,5,6,7,8,9,10]
 
 function reduceArray(prev, current, index)
 {
-  console.log("prev: " + prev);
-  console.log("current: " + current);
-  console.log("index: " + index);
-  (index + 1) % 5 ? prev.push(current) : console.log(index % 5);
-  console.log("after: " + prev);
+  if(index === 0)
+  {
+    prev.push([])
+    prev[prev.length - 1].push(current)
+  }
+  else
+  {
+    index % 5 ? prev[prev.length - 1].push(current) : ( prev.push([]) && prev[prev.length - 1].push(current) )
+  }
 
   return prev;
 }
 
-testData.reduce(reduceArray,[]);
+let number = test.reduce(reduceArray, [])
+
+let array = testData.reduce(reduceArray, [])
+
+console.log("")
+console.log("Number:")
+console.log(number)
+console.log("")
+console.log("**********************************************")
+console.log("")
+console.log("Array:")
+console.log(array)
+console.log("")
+
+
