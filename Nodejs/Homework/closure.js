5bc9cf2e71715e5ea209d778ad7b87d47f31587c
John <wei.jiang@winsuntech.cn>
2016-08-26 10:05:41 +0800

    Create closure.js

---
+++ Nodejs/Homework/closure.js
@@ -0,0 +1,19 @@
+let a = function(a){
+  let x = a + 10
+  return {
+    aaa: function(){
+      console.log("Result: " + x++)
+    }
+  }
+}
+
+let b = a(30)
+
+b.aaa()
+b.aaa()
+b.aaa()
+b.aaa()
+b.aaa()
+b.aaa()
+b.aaa()
+b.aaa()
