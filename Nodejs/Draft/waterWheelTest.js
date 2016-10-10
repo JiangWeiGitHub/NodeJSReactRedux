38414a65070cbbaf01ccd22a746872ae199e9439
John <wei.jiang@winsuntech.cn>
2016-10-10 16:32:56 +0800

    Update waterWheelTest.js

--- Nodejs/Draft/waterWheelTest.js
+++ Nodejs/Draft/waterWheelTest.js
@@ -1,53 +1,80 @@
 import request from 'superagent'
 
 const url = 'http://192.168.5.54'
-const nasUUID = 'fd1eea70-0de8-4d15-8c99-fd4f4cc9b418'
+const nasUUID = 'a1ce3758-2749-4bd6-890a-4fa905fbe7fe'
 let linkUUID = ''
 let fileName = 'chi.txt'
 let fileHash = '2a9b0963652a7647780dc13fd37d160720421ce8181dadd530e6a16203832f97'
 
-let connect = new Promise(function(resolve, reject) {
-  console.log("Connecting...")
-  request.get(`${url}/nas/${nasUUID}`)
-    .set('Accept', 'application/json')
-    .end((err, res) => {
-      if (err)
-        return resolve(err)
-
-      if (res.statusCode === 204 || res.statusCode === 304)
-        return resolve(null)
-
-      resolve(res.text)
-    })
-})
-
-let getLinkUUID = new Promise(function(resolve, reject) {
-  console.log("Getting Link UUID...")
-  request.post(`${url}/nas/${nasUUID}`)
-    .set('Accept', 'application/json')
-    .end((err, res) => {
-      if (err)
-        return resolve(err)
-
-      resolve(res.text)
-    })
-})
-
-/*
-let addNewRequest = new Promise(function(resolve, reject) {
-    request.post(`${url}/nas/${nasUUID}/waterwheel/${linkUUID}`)
+async function connect(url, nasUUID) {
+  return new Promise(function(resolve, reject) {
+    console.log("\n********** Connecting... **********")
+    request.get(`${url}/nas/${nasUUID}`)
       .set('Accept', 'application/json')
-      .send('data',fileHash)
       .end((err, res) => {
         if (err)
           return resolve(err)
 
+        console.log("Return Code: " + res.statusCode)
+
         resolve(res.text)
       })
-})
+  })
+}
 
-let uploadFileHash = new Promise(function(resolve, reject) {
+async function getLinkUUID (url, nasUUID) {
+  return new Promise(function(resolve, reject) {
+    console.log("\n********** Getting Link UUID... **********")
+    request.post(`${url}/nas/${nasUUID}`)
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        if (err)
+          return resolve(err)
+        
+        console.log("Return Code: " + res.statusCode)
+
+        resolve(res.text)
+      })
+  })
+}
+
+async function addNewRequest (url, nasUUID, linkUUID, fileHash) {
+  return new Promise(function(resolve, reject) {
+    console.log("\n********** Adding New Request... **********")
     request.post(`${url}/nas/${nasUUID}/waterwheel/${linkUUID}`)
+      .set('Accept', 'application/json')
+      .send({'data':[fileHash]})
+      .end((err, res) => {
+        if (err)
+          return resolve(err)
+
+        console.log("Return Code: " + res.statusCode)
+
+        resolve(res.text)
+      })
+  })
+}
+
+async function getTank (url, nasUUID, linkUUID) {
+  return new Promise(function(resolve, reject) {
+    console.log("\n********** Getting Tank Infor... **********")
+    request.get(`${url}/nas/${nasUUID}/waterwheel/${linkUUID}`)
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        if (err)
+          return resolve(err)
+
+        console.log("Return Code: " + res.statusCode)
+
+        resolve(res.text)
+      })
+  })
+}
+
+async function uploadFile (url, nasUUID, linkUUID, tankID, tankResourceID) {
+  return new Promise(function(resolve, reject) {
+    console.log("\n********** Uploading File... **********")
+    request.post(`${url}/nas/${nasUUID}/files/${linkUUID}/${tankID}/${tankResourceID}`)
       .set('Accept', 'application/json')
       .attach('file',fileName)
       .field('sha256',fileHash)
@@ -55,125 +82,37 @@ let uploadFileHash = new Promise(function(resolve, reject) {
         if (err)
           return resolve(err)
 
+        console.log("Return Code: " + res.statusCode)
+
         resolve(res.text)
       })
-})
-*/
-
-connect.then(function(value) {
-//  console.log("Connecting...")
-  console.log(value)
-  getLinkUUID.then(function(value) {
-//    console.log("Getting Link UUID...")
-    console.log(value)
-    linkUUID = value.slice(1,-1)
-
-    let addNewRequest = new Promise(function(resolve, reject) {
-      console.log("Adding New Request...")
-      request.post(`${url}/nas/${nasUUID}/waterwheel/${linkUUID}`)
-        .set('Accept', 'application/json')
-        .send({'data':["2a9b0963652a7647780dc13fd37d160720421ce8181dadd530e6a16203832f97"]})
-        .end((err, res) => {
-          if (err)
-            return resolve(err)
-
-            resolve(res.text)
-        })
-    })
-
-    addNewRequest.then(function(value) {
-      console.log(value)
-
-
-      let getTank = new Promise(function(resolve, reject) {
-        console.log("Getting Tank Infor...")
-        request.get(`${url}/nas/${nasUUID}/waterwheel/${linkUUID}`)
-          .set('Accept', 'application/json')
-          .end((err, res) => {
-            if (err)
-              return resolve(err)
-
-              resolve(res.text)
-          })
-      })
-
-      getTank.then(function(value) {
-        console.log(value)
-        let tankList = JSON.parse(value)
-        let len = tankList.length
-        for(let i = 0; i < len; i++) {
-          if(tankList[i].uuid == linkUUID) {
-            let tankValue = tankList[i].tanks[0]
-            console.log(tankValue)
-            let tankID = tankValue.uuid
-            let tankResourceID = tankValue.resource[0].id
-            console.log(tankID)
-            console.log(tankResourceID)
-
-
-      let uploadFile = new Promise(function(resolve, reject) {
-        console.log("Uploading File...")
-        request.post(`${url}/nas/${nasUUID}/files/${linkUUID}/${tankID}/${tankResourceID}`)
-          .set('Accept', 'application/json')
-          .attach('file',fileName)
-          .field('sha256',fileHash)
-          .end((err, res) => {
-            if (err)
-              return resolve(err)
-
-            resolve(res.text)
-          })
-      })
-
-      uploadFile.then(function(value) {
-        console.log(value)
-      })
-
-
-          }
-        }
-      })
-
-
-      /*let uploadFile = new Promise(function(resolve, reject) {
-        console.log("Uploading File...")
-        request.post(`${url}/nas/${nasUUID}/files/${linkUUID}`)
-          .set('Accept', 'application/json')
-          .attach('file',fileName)
-          .field('sha256',fileHash)
-          .end((err, res) => {
-            if (err)
-              return resolve(err)
-
-            resolve(res.text)
-          })
-      })
-
-      uploadFile.then(function(value) {
-        console.log(value)
-      })*/
-    })
-
-    /*
-    let uploadFileHash = new Promise(function(resolve, reject) {
-        request.post(`${url}/nas/${nasUUID}/waterwheel/${linkUUID}`)
-          .set('Accept', 'application/json')
-          .attach('file',fileName)
-          .field('sha256',fileHash)
-          .end((err, res) => {
-            if (err)
-              return resolve(err)
-
-            resolve(res.text)
-          })
-    })
-
-    uploadFileHash.then(function(value) {
-      console.log(value)
-    })
-    */
-
   })
+}
 
-  //console.log('Finish!')
-}).then(console.log('Finish!'))
+connect(url, nasUUID).then( function (value) {
+  console.log("Return Text: " + value)
+})
+.then( function (value) {
+  getLinkUUID(url, nasUUID).then( function (value) {
+    console.log("Return Text: " + value)
+
+    return value
+  })
+  .then( function (value) {
+    linkUUID = value.slice(1,-1)
+    addNewRequest (url, nasUUID, linkUUID, fileHash).then( function (value) {
+      console.log("Return Text: " + value)
+
+      return value
+    })
+    .then( function (value) {
+      let tankList = JSON.parse(value)
+      let tankID = tankList.uuid
+      let tankResourceID = tankList.resource[0].id
+
+      uploadFile (url, nasUUID, linkUUID, tankID, tankResourceID).then( function (value) {
+        console.log("Return Text: " + value)
+      })
+    })
+  })
+})
