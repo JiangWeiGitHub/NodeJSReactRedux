edbb206c797afa0b670ccc78cb681d7dbceaa4f9
jiangweigithub <wei.jiang@winsuntech.cn>
2016-11-16 10:52:42 +0800

    deleted:    Nodejs/Homework/AlonzoChurch.js 	deleted:    Nodejs/Homework/callback.js 	deleted:    Nodejs/Homework/class.js 	deleted:    Nodejs/Homework/closure.js 	deleted:    Nodejs/Homework/fibonacci.js 	deleted:    Nodejs/Homework/for.js 	deleted:    Nodejs/Homework/generator.js 	deleted:    Nodejs/Homework/insertDOM.js 	deleted:    Nodejs/Homework/normalizeString.js 	deleted:    Nodejs/Homework/pipeline.js 	deleted:    Nodejs/Homework/promise.js 	deleted:    Nodejs/Homework/promiseGenerator.js 	deleted:    Nodejs/Homework/reduceArray.js 	deleted:    Nodejs/Homework/stringToInt.js

--- Nodejs/Homework/callback.js
+++
@@ -1,5 +0,0 @@
-let former = (funcName) => {console.log("Former Get From Latter: " + funcName)}
-
-let latter = (string) => {console.log("Latter Contents: " + string);return "I'm latter"}
-
-former(latter("hello world"))
